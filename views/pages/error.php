072473f056136cc0086c82d27583fdbccb4686e0
sandra.jarzebska@gmail.com <sandra.jarzebska@gmail.com>
2018-04-22 19:50:09 +0100

    changed font in error page to match overall style

--- views/pages/error.php
+++ views/pages/error.php
@@ -1,6 +1,6 @@
 
 <div style="margin-left: auto; margin-right: auto; width:70% ">
     <img src="views/images/error.jpg" width='100%' />
-    <p style="margin-top: 10%; margin-bottom: 30%; font-family: Comic Sans MS, cursive, sans-serif; color: #191970; text-align: center" >Sorry. We've searched all the places in London, but we can't seem to find this page.</p>
+    <p style="margin-top: 10%; margin-bottom: 30%; font-family: Quicksand, cursive, sans-serif; color: #191970; text-align: center" >Sorry. We've searched all the places in London, but we can't seem to find this page.</p>
 
 </div>
