df4cbf860bbaf803a8074846b391b9c7d894261b
sandra.jarzebska@gmail.com <sandra.jarzebska@gmail.com>
2018-04-22 19:47:25 +0100

    added displaying article thumbnail
    - .searchResult styling added to css

--- views/articles/search.php
+++ views/articles/search.php
@@ -1,17 +1,19 @@
-<h2>Your search for <i> <?php echo $keyword ?> </i></h2>
+<h1>Your search for <i> <?php echo $keyword ?> </i></h1>
+</br>
 
-<?php
+    <?php
 $num_rows = count($list);
 if ($num_rows > 0) {
     foreach ($list as $article) {
         ?>
         <tr>
-            <td> <a href='?controller=article&action=read&readArticle_id= <?php echo $article->id; ?>'>
-        <?php echo $article->title . " "; ?> </a>published on <?php echo $article->date; ?> </br> </td>    
+            <td><img src="views/images/<?php echo $article->id . '.jpg'; ?>" class="searchResult"><a href='?controller=article&action=read&readArticle_id=<?php echo $article->id; ?>' class="searchResult">
+        <?php echo $article->title . " "; ?> </a><i>published on <?php echo $article->date; ?> </i></br> </td>    
         </tr> 
     <?php
     }
 } else {
-    echo "Your search returned no results";
+    echo '<h3>' . "Your search returned no results" . '</h3>';
 }
+
 ?>
