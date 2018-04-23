0983249078b12c223894f9b909e3f451462ef2b5
sandra.jarzebska@gmail.com <sandra.jarzebska@gmail.com>
2018-04-23 22:18:55 +0100

    footer with the subscribe function working again

--- views/blogger/login.php
+++ views/blogger/login.php
@@ -1,3 +1,14 @@
+
+<style type="text/css">
+    body {
+        margin: 0;
+        padding: 0;
+        background: url("images/londonview.jpg");
+        background-size: cover;
+        font-family: sans-serif;
+    }  
+</style>
+
 <div class='padding50px'>
     <div class='row'>
         <div class='col-md-4'>
@@ -12,23 +23,21 @@
                 <label for="password" class="col-sm-2 col-form-label">Password</label>
                 <input type="password" class="form-control" name="password"id="Password" placeholder="password">
                 <br/>
-            <center>
+                <center>
                     <button type="submit" name="submit"class="btn btn-primary">Sign in</button>
-            </center>
+                </center>
             </form>
-            </div>
-          <div class='col-md-4'>
+        </div>
+        <div class='col-md-4'>
         </div>
     </div>
 </div>
 <center>
-            <?php 
-            if ($showLoginWarning) {
-                echo '<h2>' . "Wrong username or password" . '</h2>'; 
-            }
-            
-            ?>
+    <?php
+    if ($showLoginWarning) {
+        echo '<h2>' . "Wrong username or password" . '</h2>';
+    }
+    ?>
 </center>
-            
-        
-      
+
+
