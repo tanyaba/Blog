bb12b0423b125ff0c7fb925bfc9af37fcbb302cc
TanyaB <tbadamshina@hotmail.com>
2018-04-24 16:05:16 +0100

    navbar transparent

--- views/layout.php
+++ views/layout.php
@@ -23,69 +23,73 @@
     </head> 
     <body>    
         <div class="topnavbar customnavbar">
-            <?php 
-        if ($controller=='pages' && $action=='Homepage'){
-            echo "<ul class='customnavbar'>";
-        } else {
-            echo"<ul class='customnavbarPage'>";
-        }
-        ?>
-            <!--<ul class="customnavbar">-->
-                <li><a href="index.php">Home</a></li>
-                <li><a href="?controller=article&action=readcategory">What's On</a></li>
-                <li><a href="?controller=comment&action=categorytwo">Food & Drink</a></li>
-                <li><a href="#about">Social</a></li>
-                <li><a href="#about">Sight Seeing</a></li>
-                <li><a href="#about">Hotels</a></li>
-                <div class="pull-right">
-                    <form class="searchbox" action="index.php" method="GET" role="form" id="searchform">
-                        <input type="text" name="search" id="search" class="form-control" placeholder="Search">
-                        <input type="hidden" name="controller" value="article">
-                        <input type="hidden" name="action" value="searchAll">
-                    </form>
-                </div>
-                <center>
-                    <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
-                    <li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
-                    <li><a href="#" class="icoGoogle" title="Google +"><i class="fa fa-google-plus"></i></a></li>
-                    <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
-                </center>
-            </ul>
             <?php
-            if (isset($_SESSION['blogger_id'])) {
+            if ($controller == 'pages' && $action == 'Homepage') {
                 echo "<ul class='customnavbar'>";
-                echo "<li><small><a href='?controller=article&action=readAll'>Amend articles</a></small></li> ";
-                echo "<li><small><a href='?controller=article&action=createArticle'>Create New Article</a></small></li>";
-                  echo "<li><small><a href='?controller=blogger&action=register'>Add collaborator</a></small></li>";
-                echo "<li><small><a href='?controller=blogger&action=logout'>Logout</a></small></li>";
-              
-                echo "</ul>";
+            } else {
+                echo"<ul class='customnavbarPage'>";
             }
             ?>
-        </div>
-        <div class='container main' >
-            <?php require_once('routes.php'); ?>
-        </div>
-        <div class="footer">
-            <ul>
-                <li><p style="margin-left: 10px; margin-right: 30px; margin-top: 13px; margin-bottom: 13px; color: #fff">@WhatsUpLondon2018</p></li>
-                <li><a href="#AboutUs">About Us</a></li>
-                <li><a href="?controller=pages&action=contact">Contact Us</a></li>
-                <li><a href='?controller=pages&action=unsubscribe'>Unsubscribe</a></li>
-                <li><a href='?controller=blogger&action=login'>Blogger login</a></li>
-                <div class="pull-right">
+            <!--<ul class="customnavbar">-->
+            <li><a href="index.php">Home</a></li>
+            <li><a href="?controller=article&action=readcategory">What's On</a></li>
+            <li><a href="?controller=comment&action=categorytwo">Food & Drink</a></li>
+            <li><a href="#about">Social</a></li>
+            <li><a href="#about">Sight Seeing</a></li>
+            <li><a href="#about">Hotels</a></li>
+            <div class="pull-right">
+                <form class="searchbox" action="index.php" method="GET" role="form" id="searchform">
+                    <input type="text" name="search" id="search" class="form-control" placeholder="Search">
+                    <input type="hidden" name="controller" value="article">
+                    <input type="hidden" name="action" value="searchAll">
+                </form>
+            </div>
+            <center>
+                <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#" class="icoGoogle" title="Google +"><i class="fa fa-google-plus"></i></a></li>
+                <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+            </center>
+        </ul>
+        <?php
+        if (isset($_SESSION['blogger_id'])) {
+            if ($controller == 'pages' && $action == 'Homepage') {
+                echo "<ul class='customnavbar'>";
+            } else {
+                echo"<ul class='customnavbarPage'>";
+            }
+            echo "<li><small><a href='?controller=article&action=readAll'>Amend articles</a></small></li> ";
+            echo "<li><small><a href='?controller=article&action=createArticle'>Create New Article</a></small></li>";
+            echo "<li><small><a href='?controller=blogger&action=register'>Add collaborator</a></small></li>";
+            echo "<li><small><a href='?controller=blogger&action=logout'>Logout</a></small></li>";
+
+            echo "</ul>";
+        }
+        ?>
+    </div>
+    <div class='container main' >
+        <?php require_once('routes.php'); ?>
+    </div>
+    <div class="footer">
+        <ul>
+            <li><p style="margin-left: 10px; margin-right: 30px; margin-top: 13px; margin-bottom: 13px; color: #fff">@WhatsUpLondon2018</p></li>
+            <li><a href="#AboutUs">About Us</a></li>
+            <li><a href="?controller=pages&action=contact">Contact Us</a></li>
+            <li><a href='?controller=pages&action=unsubscribe'>Unsubscribe</a></li>
+            <li><a href='?controller=blogger&action=login'>Blogger login</a></li>
+            <div class="pull-right">
                 <li><p style="margin-left: 10px; margin-right: 30px; margin-top: 13px; margin-bottom: 13px; color: #fff">Sign up to our news!</p></li>
                 <li><form style="margin-left: 10px; margin-right: 30px; margin-top: 5px; margin-bottom: 10px;" class="newsletter" method="POST" action='?controller=subscriber&action=addSubscriber'> 
-                    <input class="newsletter" type="text" placeholder="Name" name="subscriber_name" required>
-                    <input class="newsletter" type="text" placeholder="Email address" name="subscriber_email" required>
-                    <input class="newsletter" onlick= "JSalert()" type="submit" name="subscribe">
-                </form>  
+                        <input class="newsletter" type="text" placeholder="Name" name="subscriber_name" required>
+                        <input class="newsletter" type="text" placeholder="Email address" name="subscriber_email" required>
+                        <input class="newsletter" onlick= "JSalert()" type="submit" name="subscribe">
+                    </form>  
                 </li>
-                </div>
-            </ul>
+            </div>
+        </ul>
 
-        </div>
     </div>
+</div>
 </body>
 </html>                            
 
